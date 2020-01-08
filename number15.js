2851c905e7f452d5c28d48d6bf9fb7267bee2ab2
Jim Thorne <jamesthorne@Jamess-MacBook-Air.local>
2020-01-08 11:44:23 -0700

    first commit

---
+++ number15.js
@@ -0,0 +1,17 @@
+let shouldIgetAnewJob; 
+// Value is currently undefined
+// User fills out web form
+// data
+shouldIgetAnewJob = true;
+
+if (shouldIgetAnewJob) {
+    // some code to execute of user wants to get a new job
+    // TODO: Select a game a job website to go to
+
+    console.log ("Let's get a new job.");
+    //expected result "Let's get a new job"
+}
+// if the variable is changed to false the response is null
+/* The program continues to execute in another way because the condition
+was not met */
+
